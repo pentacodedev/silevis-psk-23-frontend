11a208ed67e14dd31ea1c9c5c8261b9f872d2d6f
Jacek Jarosz <jacekjarosz55@gmail.com>
2023-11-17 18:22:54 +0100

    added empty setup page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { HomeComponent } from './pages/home/home.component';
+import { SetupComponent } from './pages/setup/setup.component';
 
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http);
@@ -17,7 +18,8 @@ export function HttpLoaderFactory(http: HttpClient) {
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    SetupComponent
   ],
   imports: [
     BrowserModule,
