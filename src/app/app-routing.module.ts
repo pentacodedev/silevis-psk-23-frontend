240020107889706f1407b7796f3d26ba8d72e4da
Oskar M <mm4769305@gmail.com>
2023-11-17 19:46:26 +0100

    page update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { SetupComponent } from './pages/setup/setup.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+  },
+  {
+    path: 'setup',
+    component: SetupComponent,
   }
 ];
 
